9cc7eae7b3ebfcdd9693e7d0259e6387003c007d
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-06-23 08:09:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,15 @@ from torch import cuda
 # This file contains the configuration parameters which will be used throughout your experiments
 use_cuda = cuda.is_available()
 
-n_epochs = 30
-batch_size = 8
+n_epochs = 50
+batch_size = 16
 
 model_id = 1
 save_dir = './SavedModels/Run%d/' % model_id
 
 learning_rate = 1e-3
 weight_decay = 1e-7
+
+data_dir = '../../Datasets/CityScapes'
+
+num_workers = 8
