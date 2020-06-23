687d8f0f8d8c27fdd5b2cead9b986de62f989bb7
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-06-23 12:13:34 -0400

    Update train.py

--- train.py
+++ train.py
@@ -2,18 +2,17 @@ import config
 import torch
 from torch.autograd.variable import Variable
 import numpy as np
-from dataloader import TrainDataset, ValidationDataset, DataLoader, get_cityscapes_dataset
+from dataloader import DataLoader, get_cityscapes_dataset
 import torch.nn as nn
 import torch.optim as optim
 from deeplabv3 import DeepLabV3, Model2
 import os
-import inference
+
 
 def get_accuracy(y_pred, y):
     y_argmax = torch.argmax(y_pred, 1)
 
     return torch.mean((y_argmax.long()==y.long()).type(torch.float))
-
 
 
 def train(model, data_loader, criterion1, criterion2, optimizer):
@@ -53,8 +52,8 @@ def train(model, data_loader, criterion1, criterion2, optimizer):
         losses.append(loss.item())
         accs.append(acc.item())
 
-        if (i + 1) % 100 == 0:
-            print('Finished training %d batches. Loss: %.4f. Accuracy: %.4f.' % (i+1, float(np.mean(losses)), flo$
+        if (i + 1) % 10 == 0:
+            print('Finished training %d batches. Loss: %.4f. Accuracy: %.4f.' % (i + 1, float(np.mean(losses)), float(np.mean(accs))), flush=True)
 
     print('Finished training. Loss: %.4f. Accuracy: %.4f.' % (float(np.mean(losses)), float(np.mean(accs))))
 
@@ -102,24 +101,14 @@ def validation(model, data_loader, criterion1, criterion2):
 def run_experiment():
     # model = DeepLabV3('Model1', 'SimpleSegmentation/')
     model = Model2('Model2', 'SimpleSegmentation/')
-    if config.start_epoch != 1:
-        max_epoch = 0
-        min_loss = 10000000.0000
-        for filename in os.listdir('./SavedModels/Run%d/' % config.model_id):
-            model_info = filename.split('_')
-            epoch = model_info[1]
-            loss = model_info[2].split('.p')[0]
-            if float(loss) < min_loss and int(epoch) < config.start_epoch:
-                min_loss = float(loss)
-            if int(epoch) > max_epoch and int(epoch) < config.start_epoch:
-                max_epoch = int(epoch)
-        print('Loaded from: model_{}_{:.4f}.pth'.format(max_epoch, min_loss))
-        model.load_state_dict(torch.load(os.path.join(config.save_dir, 'model_{}_{:.4f}.pth'.format(max_epoch, min_loss)))['state_dict'])
+
     criterion1 = nn.CrossEntropyLoss(reduction='mean')
-    criterion2 = nn.MSELoss(reduction='mean')
+    criterion2 = nn.MSELoss(reduction='none')
+
     optimizer = optim.Adam(model.parameters(), lr=config.learning_rate, weight_decay=1e-7)
-    tr_dataset = get_cityscapes_dataset('~/SimpleSegmentation/CityscapesData', True, download=True) 
-    val_dataset = get_cityscapes_dataset('~/SimpleSegmentation/CityscapesData', False, download=True) 
+
+    tr_dataset = get_cityscapes_dataset(config.data_dir, True, download=True)
+    val_dataset = get_cityscapes_dataset(config.data_dir, False, download=True)
 
     best_loss = 1000000
 
@@ -128,9 +117,9 @@ def run_experiment():
 
         tr_dataloader = DataLoader(tr_dataset, batch_size=config.batch_size, shuffle=True, num_workers=config.num_workers)
         val_dataloader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers)
-        losses, _  = train(model, tr_dataloader, criterion1, criterion2, optimizer)
+        losses, _ = train(model, tr_dataloader, criterion1, criterion2, optimizer)
 
-        losses, _  = validation(model, val_dataloader, criterion1, criterion2)
+        losses, _ = validation(model, val_dataloader, criterion1, criterion2)
 
         if losses < best_loss:
             print('Model Improved -- Saving.')
@@ -150,6 +139,9 @@ def run_experiment():
             print('Model saved ', str(save_file_path))
     print('Training Finished')
 
+
 if __name__ == '__main__':
     run_experiment()
+    #inference.main()
+    print()
                                                                                    
