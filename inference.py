edbfededf5b2cd1f1de47fdb890f0200c0a3b71b
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-06-23 08:10:19 -0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,27 +1,28 @@
+from createInstanceMaps import create_instance_maps
 import config
 import numpy as np
 import torch
 from train import get_accuracy
 from torch.autograd.variable import Variable
-from torchvision.utils import save_image
 import os
-from dataloader import DataLoader, ValidationDataset, get_cityscapes_dataset
-from createInstanceMaps import create_instance_maps
-from cityscapesscripts.evaluation.evalPixelLevelSemanticLabeling import main as eval_main
-from cityscapesscripts.evaluation.evalInstanceLevelSemanticLabeling import main as eval_main2
+from dataloader import DataLoader, get_cityscapes_dataset
 from deeplabv3 import DeepLabV3, Model2
 from PIL import Image
+
+
+def mkdir(dir_name):
+    if os.path.isdir(dir_name):
+        return
+    else:
+        print('Directory %s has been created.' % dir_name)
+        os.mkdir(dir_name)
+
 
 def inference(model, data_loader):
     model.eval()
 
     if config.use_cuda:
         model.cuda()
-
-    y_list = []
-    y_pred_seg_list = []
-    y_pred_instance_list = []
-    img_ind = 0
 
     for i, sample in enumerate(data_loader):
         image, (y_gt_seg, y_gt_center, y_gt_regression, y_gt_reg_pres), name = sample
@@ -38,44 +39,55 @@ def inference(model, data_loader):
         # y_list.append(name)
 
         y_pred_seg_argmax = torch.argmax(y_pred_seg, 1)  # should give shape (B, H, W)
-        y_pred_center = y_pred_center.data.cpu().numpy() # converts tensor to numpy
-        y_pred_regression = y_pred_regression.data.cpu().numpy() #converts tensor to numpy
+
+        y_pred_seg_argmax = y_pred_seg_argmax.data.cpu().numpy()
+        y_pred_center = y_pred_center.data.cpu().numpy()
+        y_pred_regression = y_pred_regression.data.cpu().numpy()
+
         for j in range(len(y_pred_seg_argmax)):
-            img_name = './SavedImages/output_segmentation_%d.png' % img_ind
-            img = y_pred_seg_argmax[j].cpu().data.numpy()  # Converts the tensor to numpy
-            img  = Image.fromarray(img) # Converts numpy array to PIL Image
-            img = img.resize(size=(512, 1024), resample=Image.NEAREST) # Resizes image
-            img.save(img_name) # Saves image
-            y_pred_seg_list.append(img_name)
+            img_name_split = name[j].split('/')
+            city = img_name_split[-2]
+            mkdir('./SavedImages/val/Pixel/' + city)
+
+            img_name = './SavedImages/val/Pixel/' + city + '/' + img_name_split[-1].replace('leftImg8bit', 'predFine_labelids')
+
+            img = Image.fromarray(y_pred_seg_argmax[j].astype(np.uint8), mode='P')  # Converts numpy array to PIL Image
+            img = img.resize(size=(2048, 1024), resample=Image.NEAREST)  # Resizes image
+            img.save(img_name, "PNG", mode='P')  # Saves image
+
             pred_instance_map = create_instance_maps(y_pred_seg_argmax[j], y_pred_center[j], y_pred_regression[j])
-            img_name = './SavedImages/output_instances_%d.png' % img_ind
-            img  = Image.fromarray(pred_instance_map) # Converts numpy array to PIL Image
-            img = img.resize(size=(512, 1024), resample=Image.NEAREST) # Resizes image
-            img.save(img_name) # Saves image
-            img_ind += 1
-            y_pred_instance_list.append(img_name)
-            y_list.append(name[j])
-        if i == 5:
-            break
-    eval_main(y_pred_seg_list, y_list)
-    eval_main2(y_pred_instance_list, y_list)
+
+            mkdir('./SavedImages/val/Instance/' + city)
+
+            img_name = './SavedImages/val/Instance/' + city + '/' + img_name_split[-1].replace('leftImg8bit', 'predFine_instanceids')
+
+            img = Image.fromarray(pred_instance_map, mode='L')  # Converts numpy array to PIL Image
+            img = img.resize(size=(2048, 1024), resample=Image.NEAREST)  # Resizes image
+            img.save(img_name, 'PNG', mode='L')  # Saves image
+
+        if (i+1) % 5 == 0:
+            print('Finished %d batches' % (i+1), flush=True)
+
 
 def main():
-    max_epoch = 0
-    min_loss = 1000000.0000
-    for filename in os.listdir('./SavedModels/Run%d/' % config.model_id):
-        model_info = filename.split('_')
-        epoch = model_info[1]
-        loss = model_info[2].split('.p')[0]
-        if float(loss) < min_loss:
-            min_loss = float(loss)
-        if int(epoch) > max_epoch:
-            max_epoch = int(epoch)
-        model = Model2('Model2', 'SimpleSegmentation/')
-        model.load_state_dict(torch.load(os.path.join(config.save_dir, 'model_{}_{:.4f}.pth'.format(max_epoch, min_loss)))['state_dict'])
-        val_dataset = get_cityscapes_dataset('~/SimpleSegmentation/CityscapesData', False, download=True)
-        val_dataloader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False)
+    mkdir('./SavedImages/')
+    mkdir('./SavedImages/val/')
+    mkdir('./SavedImages/val/Pixel/')
+    mkdir('./SavedImages/val/Instance/')
 
-        inference(model, val_dataloader)
+    # model_15_32.0749.pth
+
+    epoch = 15
+    loss = 32.0749
+
+    model = Model2('Model2', 'SimpleSegmentation/')
+    model.load_state_dict(torch.load(os.path.join(config.save_dir, 'model_{}_{:.4f}.pth'.format(epoch, loss)))['state_dict'])
+
+    val_dataset = get_cityscapes_dataset(config.data_dir, False, download=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers)
+
+    inference(model, val_dataloader)
 
 
+if __name__ == '__main__':
+    main()
