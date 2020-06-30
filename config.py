0b35777c8e2e3144ac57cb59269e3bc505affec4
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-06-30 09:48:58 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
+
 from torch import cuda
 
 # This file contains the configuration parameters which will be used throughout your experiments
 use_cuda = cuda.is_available()
 
 start_epoch = 1
-n_epochs = 50
+n_epochs = 10
 batch_size = 16
+use_instance = False
+
+seg_coef = 1
+center_coef = 1
+regression_coef = 0.01
 
 model_id = 1
 save_dir = './SavedModels/Run%d/' % model_id
@@ -13,6 +19,6 @@ save_dir = './SavedModels/Run%d/' % model_id
 learning_rate = 1e-3
 weight_decay = 1e-7
 
-data_dir = '../../Datasets/CityScapes'
+data_dir = './CityscapesData'
 
 num_workers = 8
