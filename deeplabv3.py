9b50edeef3d0033798767e4b9e5e1b36a2c95509
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-06-30 09:49:57 -0400

    Update deeplabv3.py

--- deeplabv3.py
+++ deeplabv3.py
@@ -1,4 +1,4 @@
-
+from decoder import seg_decoder, inst_decoder
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -53,9 +53,11 @@ class Model2(nn.Module):
         self.project_dir = project_dir
         self.create_model_dirs()
 
-        self.resnet = ResNet50_OS16() # NOTE! specify the type of ResNet here
-        self.aspp_seg = ASPP_Bottleneck(num_classes=self.num_classes) # NOTE! if you use ResNet50-152, set self.aspp = ASPP_Bottleneck(num_classes=self.num_classes) instead
-        self.aspp = ASPP2_Bottleneck(num_classes=self.num_classes) 
+        self.resnet = ResNet101_OS16() # NOTE! specify the type of ResNet here
+        # self.aspp_seg = ASPP(num_classes=self.num_classes) # NOTE! if you use ResNet50-152, set self.aspp = ASPP_Bottleneck(num_classes=self.num_classes) instead
+        # self.aspp = ASPP_2() # for ResNet18 and ResNet34
+        self.aspp_seg = ASPP_Bottleneck(num_classes = self.num_classes) # for ResNet50 and ResNet101
+        self.aspp = ASPP2_Bottleneck(num_classes=self.num_classes) # for ResNet50 and ResNet101
 
     def forward(self, x):
         # (x has shape (batch_size, 3, h, w))
@@ -65,11 +67,11 @@ class Model2(nn.Module):
         # Encoder:
         feature_map = self.resnet(x) # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNet18_OS8 or ResNet34_OS8, it will be (batch_size, 512, h/8, w/8). If self.resnet is ResNet50-152, it will be (batch_size, 4*512, h/16, w/16))
 
-        # Decoder for instance segmentation:
+        # Decoder for semantic segmentation:
         output = self.aspp_seg(feature_map) # (shape: (batch_size, num_classes, h/16, w/16))
         output = F.upsample(output, size=(h, w), mode="bilinear") # (shape: (batch_size, num_classes, h, w))
 
-
+        # Decoder for instance segmentation:
         center, regressions = self.aspp(feature_map)
         center = F.sigmoid(center)
         center = F.upsample(center, size=(h, w), mode="bilinear")
@@ -88,3 +90,51 @@ class Model2(nn.Module):
         if not os.path.exists(self.model_dir):
             os.makedirs(self.model_dir)
             os.makedirs(self.checkpoints_dir)
+
+
+class Model3(nn.Module):
+    def __init__(self, model_id, project_dir):
+        super(Model3, self).__init__()
+        self.num_classes = 34
+        self.model_id = model_id
+        self.project_dir = project_dir
+        self.create_model_dirs()
+
+        self.resnet = ResNet101_OS16() # NOTE! specify the type of ResNet here
+        # self.aspp_seg = ASPP(num_classes=self.num_classes) # NOTE! if you use ResNet50-152, set self.aspp = ASPP_Bottleneck(num_classes=self.num_classes) in$
+        # self.aspp = ASPP_2() # for ResNet18 and ResNet34
+        self.aspp_seg = ASPP_Bottleneck(num_classes = self.num_classes) # for ResNet50 and ResNet101
+        self.aspp = ASPP2_Bottleneck(num_classes=self.num_classes) # for ResNet50 and ResNet101
+
+    def forward(self, x):
+        # (x has shape (batch_size, 3, h, w))
+        h = x.size()[2]
+        w = x.size()[3]
+
+        # Encoder:
+        feature_map = self.resnet(x) # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNe$
+
+        # Decoder for semantic segmentation:
+        output = self.aspp_seg(feature_map) # (shape: (batch_size, num_classes, h/16, w/16))
+        output = seg_decoder(output)
+
+        # Decoder for instance segmentation:
+        center, regressions = self.aspp(feature_map)
+        center = F.sigmoid(center)
+        center = inst_decoder(center)
+        regressions = inst_decoder(regressions)
+
+        # Should output center with shape (B, 1, H/16, W/16)
+        # and regressions with shape(B, 2, H/16, W/16)
+        return output, center, regressions
+
+    def create_model_dirs(self):
+        self.logs_dir = self.project_dir + "/training_logs"
+        self.model_dir = self.logs_dir + "/model_%s" % self.model_id
+        self.checkpoints_dir = self.model_dir + "/checkpoints"
+        if not os.path.exists(self.logs_dir):
+            os.makedirs(self.logs_dir)
+        if not os.path.exists(self.model_dir):
+            os.makedirs(self.model_dir)
+            os.makedirs(self.checkpoints_dir)
+
