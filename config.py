406623744e2f0dc4d5b6c2c5435b1996ba00ac7d
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-08-13 18:16:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,34 @@ from torch import cuda
 # This file contains the configuration parameters which will be used throughout your experiments
 use_cuda = cuda.is_available()
 
-start_epoch = 1
-n_epochs = 300
-batch_size = 16
-use_instance = True
+start_iteration = 0
+n_iterations = 120000
 
-seg_coef = 1
-center_coef = 1
-regression_coef = 0.01
+save_every_n_iters = 1000
 
-model_id = 6
+batch_size = 8
+
+n_classes = 34
+
+model = 'Model'  # 'CapsuleModel'
+
+model_id = 3
 save_dir = './SavedModels/Run%d/' % model_id
-save_dir2 = './SavedIterations/Run%d/' % model_id
 
 learning_rate = 1e-3
-weight_decay = 1e-7
-h = 1024
-w = 2048
+weight_decay = 0.0
+
+poly_lr_scheduler = True
+use_dropout = True
 
 data_dir = './CityscapesData'
 
 num_workers = 8
-n_classes = 34
+
+seg_coef = 1.0
+center_coef = 1.0
+regression_coef = 0.01
+
+h, w = 512, 1024
+
+use_instance = True
