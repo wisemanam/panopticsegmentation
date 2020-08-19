1f7ce0c6b2381bba77471e00f47504a3da91a4bd
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-08-19 13:36:50 -0400

    Update train.py

--- train.py
+++ train.py
@@ -99,7 +99,7 @@ def train(model, data_loader, criterion1, criterion2, criterion3, optimizer, ite
 def run_experiment():
     if config.model == 'CapsuleModel':
         model = CapsuleModel2('CapsuleModel2', 'SimpleSegmentation/')
-        criterion1 = MarginLoss(reduction='none', ignore_index=255)
+        criterion1 = nn.CrossEntropyLoss(reduction='none', ignore_index=255)
     else:
         model = Model3('Model3', 'SimpleSegmentation/')
         criterion1 = nn.CrossEntropyLoss(reduction='none', ignore_index=255)
