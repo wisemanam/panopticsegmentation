f2e548030b958d499744ae548d40a900f03fdee6
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-08-23 13:42:10 -0400

    Update deeplabv3.py

--- deeplabv3.py
+++ deeplabv3.py
@@ -8,6 +8,7 @@ from resnet import ResNet18_OS16, ResNet34_OS16, ResNet50_OS16, ResNet101_OS16, 
 from aspp import ASPP, ASPP_Bottleneck
 from decoder import seg_decoder, inst_decoder, by_regression_inst_decoder, seg_decoder2
 from capsules import PrimaryCaps, ConvCaps, CapsulePooling
+from HoughCapsules import HoughRouting1
 
 
 class Model(nn.Module):
@@ -245,18 +246,20 @@ class CapsuleModel2(nn.Module):
         self.aspp = ASPP_Bottleneck()
 
         in_feats = 1280
-  
-        self.primary_caps = PrimaryCaps(in_feats, 32, (1, 1))
-        self.caps_pooling = CapsulePooling((3,3), (1, 1), (1, 1))
 
-        self.class_capsules = ConvCaps(32, 16, (1,1), padding=None)
-            
+        self.primary_caps = PrimaryCaps(in_feats, 32, (1, 1))
+        self.caps_pooling = CapsulePooling((3, 3), (1, 1), (1, 1))
+
+        self.class_capsules = ConvCaps(32, 16, (1, 1), padding=None)
+
         self.segmentation_decoder = seg_decoder2(in_feats=in_feats, num_classes=self.num_classes)
         self.instance_decoder = inst_decoder(in_feats=in_feats)
-        
+
         self.linear = nn.Linear(in_feats, self.num_classes)
 
-    def forward(self, x, point_lists, gt_seg=None):
+        self.hough_routing = HoughRouting1()
+
+    def forward(self, x, point_lists=None, gt_seg=None):
         # (x has shape (batch_size, 3, h, w))
         h = x.size()[2]
         w = x.size()[3]
@@ -265,22 +268,9 @@ class CapsuleModel2(nn.Module):
         feature_map, skip_8, skip_4 = self.resnet(x)  # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNe$
 
         # Capsules
-        output = self.aspp(feature_map)  # (shape: (batch_size, 256, h/16, w/16))
+        feature_output = self.aspp(feature_map)  # (shape: (batch_size, 256, h/16, w/16))
 
-        # primary_capsules = self.primary_caps(output)  # (batch_size, h/16, w/16, 32*(4*4+1))
-
-        # primary_capsules_pooled = self.caps_pooling(primary_capsules)
-
-        # class_capsules = self.class_capsules(primary_capsules_pooled)  # (batch_size, h/16, w/16, C*(4*4+1))
-
-        # b, h_down, w_down, _ = class_capsules.shape
-        # c = 16
-        # p = 4
-
-        # poses, activations = class_capsules[..., :c*p*p], class_capsules[..., c*p*p:]  # Shapes (batch_size, h/16, w/16, C*4*4) and (batch_size, h/16, w/16, C)
-        # poses = poses.permute(0, 3, 1, 2).contiguous()
-
-        class_capsules, poses = output, output   # we use the output of the aspp as our "capsules"
+        class_capsules, poses = feature_output, feature_output  # we use the output of the aspp as our "capsules"
 
         # Decoder for semantic segmentation:
         output = self.segmentation_decoder(poses, skip_8, skip_4)
@@ -297,16 +287,20 @@ class CapsuleModel2(nn.Module):
         regressions[:, 0] = regressions[:, 0] * w
         regressions[:, 1] = regressions[:, 1] * h
 
+        if point_lists is None:
+            inst_maps, point_lists, segmentation_lists = self.hough_routing(output, regressions, gt_seg)
+        else:
+            inst_maps = []
+            segmentation_lists = []
 
         class_outputs = []
         for i, point_list in enumerate(point_lists):
-            
+
             class_outs = []
             for inst_points in point_list:
-
                 # gather capsules corresponding to inst_points
-                inst_points = torch.unique(inst_points//16, dim=1)
-                 
+                inst_points = torch.unique(inst_points // 16, dim=1)
+
                 y_coords, x_coords = inst_points[0, :], inst_points[1, :]
 
                 # perform routing on inst capsules to get class capsules
@@ -318,14 +312,14 @@ class CapsuleModel2(nn.Module):
                 # get activations from the class capsules
                 # class_output = F.sigmoid(linear_class_capsules)
                 class_output = F.softmax(linear_class_capsules, dim=-1)
-                
+
                 class_outs.append(class_output)
 
             class_outputs.append(torch.stack(class_outs) if len(class_outs) != 0 else [])
 
         # Should output center with shape (B, 1, H/16, W/16)
         # and regressions with shape(B, 2, H/16, W/16)
-        return output, center, regressions, class_outputs
+        return output, center, regressions, class_outputs, inst_maps, segmentation_lists
 
     def create_model_dirs(self):
         self.logs_dir = self.project_dir + "/training_logs"
@@ -336,7 +330,105 @@ class CapsuleModel2(nn.Module):
         if not os.path.exists(self.model_dir):
             os.makedirs(self.model_dir)
             os.makedirs(self.checkpoints_dir)
-        
+
+class CapsuleModel3(nn.Module):
+    def __init__(self, model_id, project_dir):
+        super(CapsuleModel3, self).__init__()
+        self.num_classes = config.n_classes
+        self.model_id = model_id
+        self.project_dir = project_dir
+        self.create_model_dirs()
+
+        self.resnet = ResNet50_OS16()  # NOTE! specify the type of ResNet here
+
+        self.aspp = ASPP_Bottleneck()
+
+        in_feats = 1280
+
+        self.primary_caps = PrimaryCaps(in_feats, 32, (1, 1))
+        self.caps_pooling = CapsulePooling((3, 3), (1, 1), (1, 1))
+
+        self.class_capsules = ConvCaps(32, 16, (1, 1), padding=None)
+
+        self.segmentation_decoder = seg_decoder2(in_feats=in_feats, num_classes=self.num_classes)
+        self.instance_decoder = inst_decoder(in_feats=in_feats)
+
+        self.linear = nn.Linear(in_feats, self.num_classes)
+
+        self.hough_routing = HoughRouting1()
+
+    def forward(self, x, point_lists=None, gt_seg=None):
+        # (x has shape (batch_size, 3, h, w))
+        h = x.size()[2]
+        w = x.size()[3]
+
+        # Encoder:
+        feature_map, skip_8, skip_4 = self.resnet(x)  # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNe$
+
+        # Capsules
+        feature_output = self.aspp(feature_map)  # (shape: (batch_size, 256, h/16, w/16))
+
+        class_capsules, poses = feature_output, feature_output  # we use the output of the aspp as our "capsules"
+
+        # Decoder for semantic segmentation:
+        output = self.segmentation_decoder(poses, skip_8, skip_4)
+        output = F.sigmoid(output)
+
+        # Decoder for instance segmentation:
+        center, regressions = self.instance_decoder(poses, skip_8, skip_4)
+        center = F.sigmoid(center)
+        regressions = F.tanh(regressions)
+
+        output = F.upsample(output, size=(h, w), mode="bilinear")  # (shape: (batch_size, num_classes, h, w))
+        center = F.upsample(center, size=(h, w), mode="bilinear")
+        regressions = F.upsample(regressions, size=(h, w), mode="bilinear")
+        regressions[:, 0] = regressions[:, 0] * w
+        regressions[:, 1] = regressions[:, 1] * h
+
+        if point_lists is None:
+            inst_maps, point_lists, segmentation_lists = self.hough_routing(output, regressions, gt_seg)
+        else:
+            inst_maps = []
+            segmentation_lists = []
+
+        class_outputs = []
+        for i, point_list in enumerate(point_lists):
+
+            class_outs = []
+            for inst_points in point_list:
+                # gather capsules corresponding to inst_points
+                inst_points = torch.unique(inst_points // 16, dim=1)
+
+                y_coords, x_coords = inst_points[0, :], inst_points[1, :]
+
+                # perform routing on inst capsules to get class capsules
+                inst_capsules = class_capsules[i, :, y_coords, x_coords]
+                inst_capsules = torch.transpose(inst_capsules, 0, 1)
+                linear_class_capsules = self.linear(inst_capsules)
+                linear_class_capsules = torch.mean(linear_class_capsules, 0)
+
+
+                # get activations from the class capsules
+                class_output = F.softmax(linear_class_capsules, dim=-1)
+
+                class_outs.append(class_output)
+
+            class_outputs.append(torch.stack(class_outs) if len(class_outs) != 0 else [])
+
+        # Should output center with shape (B, 1, H/16, W/16)
+        # and regressions with shape(B, 2, H/16, W/16)
+        return output, center, regressions, class_outputs, inst_maps, segmentation_lists
+
+    def create_model_dirs(self):
+        self.logs_dir = self.project_dir + "/training_logs"
+        self.model_dir = self.logs_dir + "/model_%s" % self.model_id
+        self.checkpoints_dir = self.model_dir + "/checkpoints"
+        if not os.path.exists(self.logs_dir):
+            os.makedirs(self.logs_dir)
+        if not os.path.exists(self.model_dir):
+            os.makedirs(self.model_dir)
+            os.makedirs(self.checkpoints_dir)
+
 class Model3(nn.Module): # gets regressions by class
     def __init__(self, model_id, project_dir):
         super(Model3, self).__init__()
