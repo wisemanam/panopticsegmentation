782c4993d400bdd468ee4741899d4bc0b52f994b
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-09-16 19:45:44 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,25 @@ use_cuda = cuda.is_available()
 start_iteration = 0
 n_iterations = 120000
 
-save_every_n_iters = 1000
+save_every_n_iters = 2000
 
-batch_size = 8
+batch_size = 12
 
 n_classes = 34
 
-model = 'CapsuleModel'  # 'CapsuleModel'
+model = 'CapsuleModelNew1'  # 'CapsuleModel'
 
-model_id = 3
+# model_id = 1 # CapsuleModelNew1 new positional encoding add to final dimensions 
+# model_id = 2 # CapsuleModelNew1 new positional encoding concat
+
+model_id = 3 # CapsuleModelNew1, multiply acts*poses
+# model_id = 4 # CapsuleModelNew1, baseline batch_size=8
+# model_id = 5 # CapsulesModelNewLayers, multiple layers batch_size=12 
+
+# model_id = 6 # CapsuleModelNew1 concatenate batch_size=12
+# model_id = 7 # CapsuleModelNew1 add the final two dimensions batch_size=12
+
+
 save_dir = './SavedModels/Run%d/' % model_id
 
 learning_rate = 1e-3
@@ -22,6 +32,7 @@ weight_decay = 0.0
 
 poly_lr_scheduler = True
 use_dropout = True
+positional_encoding = False
 
 data_dir = './CityscapesData'
 
@@ -29,7 +40,8 @@ num_workers = 8
 
 seg_coef = 1.0
 center_coef = 0
-regression_coef = 0.01
+regression_coef = 1.0
+class_coef = 1.0
 
 h, w = 512, 1024
 
