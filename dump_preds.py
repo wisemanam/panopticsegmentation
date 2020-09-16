8e6653d30c1e9213592f64db1ef0831b652fa4e3
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-09-16 19:54:15 -0400

    Update dump_preds.py

--- dump_preds.py
+++ dump_preds.py
@@ -1,11 +1,10 @@
 import config
 import numpy as np
 import torch
-from train import get_accuracy
-from torch.autograd.variable import Variable
+from torch import nn
 import os
 from dataloader import DataLoader, get_cityscapes_dataset, custom_collate
-from deeplabv3 import Model, CapsuleModel, Model2, Model3, CapsuleModel2
+from modelNew import CapsuleModelNew1, CapsuleModelNewLayers
 from PIL import Image
 
 def mkdir(dir_name):
@@ -16,93 +15,109 @@ def mkdir(dir_name):
         os.mkdir(dir_name)
 
 
-def convert_train_id_to_eval_id(prediction):
-    # Shape (B, C, H, W)
-    b, c, h, w = prediction.shape
-    eval_preds = torch.zeros((b, 34, h, w), dtype=prediction.dtype)  # TODO check if this should be zero or -infinity
+class Converter(nn.Module):
+    def __init__(self, dims=(512, 1024)):
+        super(Converter, self).__init__()
 
-    _CITYSCAPES_TRAIN_ID_TO_EVAL_ID = [7, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33]
+        self.zero_vals = -1000000.0
+        h, w = dims
+        eval_preds = torch.ones((1, 34, h, w), dtype=torch.float)
 
-    eval_preds[:, _CITYSCAPES_TRAIN_ID_TO_EVAL_ID] = prediction
+        self.register_buffer('eval_preds', eval_preds)
 
-    return eval_preds
+    def forward(self, prediction):
+        # Shape (B, C, H, W)
+        b, _, _, _ = prediction.shape
+        eval_preds = (self.eval_preds.repeat((b, 1, 1, 1)))*self.zero_vals
 
+        _CITYSCAPES_TRAIN_ID_TO_EVAL_ID = [7, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33]
+
+        eval_preds[:, _CITYSCAPES_TRAIN_ID_TO_EVAL_ID] = prediction
+
+        return eval_preds
+        
 
 def inference(model, data_loader):
     model.eval()
+    convert_to_eval = Converter(dims=(config.h, config.w))
 
     if config.use_cuda:
         model.cuda()
+        convert_to_eval.cuda()
 
-    n_samples = 0
     for i, sample in enumerate(data_loader):
-        # image, (y_gt_seg, y_gt_center, y_gt_regression, y_gt_reg_pres, _), name = sample
-        image, (y_gt_seg, y_gt_center, y_gt_regression, y_gt_reg_pres, _), gt_class_list, gt_point_list, name = sample
+        image, (y_gt_seg, y_gt_center, y_gt_regression, y_gt_reg_pres, segmentation_weights), gt_class_list, gt_point_list, img_name = sample
 
         if config.use_cuda:
             image = image.cuda()
             y_gt_seg = y_gt_seg.cuda()
             y_gt_center = y_gt_center.cuda()
             y_gt_regression = y_gt_regression.cuda()
+            y_gt_reg_pres = y_gt_reg_pres.cuda()
 
         with torch.no_grad():
-            # y_pred_seg, y_pred_center, y_pred_regression = model(image)
-            # y_pred_seg, y_pred_center, y_pred_regression, pred_class_list = model(image, gt_point_list, y_gt_seg) # if using CapsuleModel2
-            y_pred_seg, y_pred_center, y_pred_regression, pred_class_list, inst_maps, y_pred_segmentation_lists = model(image, gt_point_list, y_gt_seg)
+            y_pred_fg_seg, y_pred_center, y_pred_regressions, y_pred_class, inst_maps, segmentation_lists = model(image, None, None)
 
-            y_gt_seg = y_gt_seg.data.cpu().numpy()
-            y_gt_center = y_gt_center.data.cpu().numpy()
-            y_gt_regression = y_gt_regression.data.cpu().numpy()
-            y_pred_seg = y_pred_seg.data.cpu().numpy()
-            y_pred_center = y_pred_center.data.cpu().numpy()
-            y_pred_regression = y_pred_regression.data.cpu().numpy()
+            # if config.n_classes == 19:  # TODO implement the class conversion later
+            #     y_pred_class = convert_to_eval(y_pred_class)
 
-            for j in range(len(y_gt_seg)):
-                img_name_split = name[j].split('/')
-                city = img_name_split[-2]
-                mkdir('./DumpedOutputs/' + city)
 
-                img_name = './DumpedOutputs/' + city + '/' + img_name_split[-1].replace('_leftImg8bit.png', '')
-                np.save(img_name + '_gt_seg.npy', y_gt_seg[j])
+        for j in range(len(y_pred_fg_seg)):
+            img_name_split = img_name[j].split('/')
+            city = img_name_split[-2]
 
-                img_name = './DumpedOutputs/' + city + '/' + img_name_split[-1].replace('_leftImg8bit.png', '')
-                np.save(img_name + '_gt_center.npy', y_gt_center[j])
+            mkdir('./SavedImages/val/Instance/' + city)
+            inst_dir_name = './SavedImages/val/Instance/' + city + '/' + img_name_split[-1].replace('leftImg8bit', '')[:-5] + '/'
+            mkdir(inst_dir_name)
 
-                img_name = './DumpedOutputs/' + city + '/' + img_name_split[-1].replace('_leftImg8bit.png', '')
-                np.save(img_name + '_gt_regression.npy', y_gt_regression[j])
+            class_probs = y_pred_class[j]  # Shape (N, C)
 
-                img_name = './DumpedOutputs/' + city + '/' + img_name_split[-1].replace('_leftImg8bit.png', '')
-                np.save(img_name + '_pr_seg.npy', y_pred_seg[j])
+            if len(class_probs) != 0:
+                class_probs = class_probs.cpu()
 
-                img_name = './DumpedOutputs/' + city + '/' + img_name_split[-1].replace('_leftImg8bit.png', '')
-                np.save(img_name + '_pr_center.npy', y_pred_center[j])
+                class_preds = np.argmax(class_probs, -1)
 
-                img_name = './DumpedOutputs/' + city + '/' + img_name_split[-1].replace('_leftImg8bit.png', '')
-                np.save(img_name + '_pr_regression.npy', y_pred_regression[j])
+                segmentation_list = segmentation_lists[j]  # length N
 
-                img_name = './DumpedOutputs/' + city + '/' + img_name_split[-1].replace('_leftImg8bit.png', '')
-                np.save(img_name + '_pr_inst_maps.npy', inst_maps[j])
+            lines = []
+            for inst in range(len(class_probs)):
+                binary_map = segmentation_list[inst]
+                inst_class = class_preds[inst]
+                inst_prob = 1.0
+                seg_prob = class_probs[inst, inst_class]
 
-                n_samples += 1
-                if n_samples == 100:
-                    exit()
+                binary_map = binary_map.data.cpu().numpy()
+
+                img = Image.fromarray(binary_map, mode='L')  # Converts numpy array to PIL Image
+                img = img.resize(size=(2048, 1024), resample=Image.NEAREST)  # Resizes image
+                img.save(inst_dir_name + 'instance%d.png' % inst, 'PNG', mode='L')  # Saves image
+
+                line_str = (inst_dir_name + 'instance%d.png'% inst).replace('./SavedImages/val/Instance/', '')
+
+                line_str = line_str + (' %d %.4f\n' % (inst_class, inst_prob*seg_prob))
+
+                lines.append(line_str)
+
+            file_dir_name = './SavedImages/val/Instance/' + img_name_split[-1].replace('leftImg8bit', '')[:-5] + '.txt'
+            with open(file_dir_name, 'w') as f:
+                f.writelines(lines)
 
         if (i+1) % 5 == 0:
             print('Finished %d batches' % (i+1), flush=True)
 
 
 def main():
-    mkdir('./DumpedOutputs/')
+    mkdir('./SavedImages/')
+    mkdir('./SavedImages/val/')
+    mkdir('./SavedImages/val/Pixel/')
+    mkdir('./SavedImages/val/Instance/')
 
-    # model_60_0.4552.pth model_50_20.2748.pth
+    iteration = 120000
 
-    iteration = 20000
-
-    # model_10_1.2785.pth
-    if config.model == 'CapsuleModel':
-        model = CapsuleModel2('CapsuleModel2', 'SimpleSegmentation/')
-    else:
-        model = Model3('Model3', 'SimpleSegmentation/')
+    if config.model == 'CapsuleModelNew1':
+        model = CapsuleModelNew1('CapsuleModelNew1', 'SimpleSegmentation/')
+    elif config.model == 'CapsuleModelNewLayers':
+        model = CapsuleModelNewLayers('CapsuleModelNewLayers', 'SimpleSegmentation/')
 
     model.load_state_dict(torch.load(os.path.join(config.save_dir, 'model_iteration_{}.pth'.format(iteration)))['state_dict'])
 
@@ -110,7 +125,6 @@ def main():
     val_dataloader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers, collate_fn=custom_collate)
 
     inference(model, val_dataloader)
-
 
 
 if __name__ == '__main__':
