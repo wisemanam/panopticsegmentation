527f18c94b4950cc258165d6248e05b471e2ae79
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-11-16 09:24:25 -0500

    Update modelNew.py

--- modelNew.py
+++ modelNew.py
@@ -445,20 +445,12 @@ class CapsuleModel6(nn.Module):
 
         self.transformer_routing2 = TransformerRouting(n_feats_in=self.vote_dim, n_caps_out=self.num_inst_classes, output_dim=16, use_vote_transform=False)
         
-    def forward(self, x, point_lists=None, gt_seg=None, gt_reg=None, two_stage=False):
-        # (x has shape (batch_size, 3, h, w))
-        h = x.size()[2]
-        w = x.size()[3]
-
-        # Encoder:
-        feature_map, skip_8, skip_4 = self.resnet(x)  # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNe$
-
-        feature_output = self.aspp(feature_map)  # (shape: (batch_size, 1280, h/16, w/16))
-
+    def get_primary_and_fg_capsules(self, feature_output, skip_8, skip_4, (h, w)):
+    
         # creates the first capsule layer
         capsule_poses_s4, capsule_acts_s4 = self.primary_capsules_skip_4(skip_4)
         capsule_poses_s8, capsule_acts_s8 = self.primary_capsules_skip_8(skip_8)
-
+    
         capsule_poses, capsule_acts = self.primary_capsules(feature_output)
 
         capsule_votes_seg_s4 = self.vote_transform_seg_skip_4(capsule_poses_s4)  # (batch_size, n_caps*vote_dim, h/4, w/4)
@@ -480,33 +472,25 @@ class CapsuleModel6(nn.Module):
         capsule_acts_seg = torch.cat((capsule_acts_up, capsule_acts_up_s8, capsule_acts_s4), 1).permute(0, 2, 3, 1) # (B, h, w, 16)
 
         fgbg_poses, fgbg_acts = self.transformer_routing_seg(capsule_votes_seg, capsule_acts_seg)
-
-        fg_pred = fgbg_acts[..., 0].unsqueeze(1)  # Shape (batch_size, 1, h/16, w/16)
-
-        fg_poses = fgbg_poses[..., 0, :]
-        regressions = self.regression_linear(fg_poses).permute(0, 3, 1, 2)
-        regressions = F.tanh(regressions)
-
-        # Resizes the output maps
-        fg_pred = F.upsample(fg_pred, size=(h, w), mode="bilinear")  # (shape: (batch_size, num_classes, h, w))
-        center = torch.zeros_like(fg_pred)  # F.upsample(center, size=(h, w), mode="bilinear")
-        regressions = F.upsample(regressions, size=(h, w), mode="bilinear")
-        regressions[:, 0] = regressions[:, 0] * w
-        regressions[:, 1] = regressions[:, 1] * h
-
-        capsule_votes_class = self.vote_transform_class(capsule_poses)  # (batch_size, n_caps*vote_dim, h/16, w/16)
-
+    
+    def create_inst_maps(self, point_lists, gt_reg, gt_seg, fg_pred, regressions):
         if point_lists is None:
             # inst_maps, point_lists, segmentation_lists = self.hough_routing(fg_pred, regressions, gt_seg)
             if gt_reg is None:
-                inst_maps, point_lists1, segmentation_lists = self.hough_routing(fg_pred, regressions, gt_seg)
+                inst_maps, point_lists, segmentation_lists = self.hough_routing(fg_pred, regressions, gt_seg)
             else:
-                inst_maps, point_lists1, segmentation_lists = self.hough_routing(fg_pred, gt_reg, gt_seg)
+                inst_maps, point_lists, segmentation_lists = self.hough_routing(fg_pred, gt_reg, gt_seg)
         else:
             inst_maps = []
             segmentation_lists = []
-            point_lists1 = point_lists
-        class_outputs = []
+            
+        return point_lists, inst_maps, segmentation_lists
+        
+    def scatter_capsules(self, point_lists, capsule_votes_inst, capsule_acts, (h, w), instance_scale):
+        b_size, _, h_inp, w_inp = capsule_votes_inst.shape
+        assert h//h_inp == w/w_inp
+        capsule_scale = h//h_inp
+    
         instance_poses = torch.zeros((b_size, h//4, w//4, self.num_inst_classes, 16))
         instance_acts = torch.zeros((b_size, h//4, w//4, self.num_inst_classes))
         
@@ -557,108 +541,144 @@ class CapsuleModel6(nn.Module):
 
             class_outputs.append(torch.stack(class_outs) if len(class_outs) != 0 else [])
             
-            fg_preds, reg_preds, class_preds, inst_maps_preds, seg_list_preds = [], [], [], [], []
+        return instance_poses, instance_acts, class_outputs
+        
+    def scatter_capsules2(self, point_lists, capsule_votes_inst, capsule_acts, input_size):
+        h, w = input_size
+        b_size, _, h_inp, w_inp = capsule_votes_inst.shape
+        assert h//h_inp == w/w_inp
+        capsule_scale = h//h_inp
+
+        class_outputs = []
+        for i, point_list in enumerate(point_lists):
+
+            class_outs = []
+            for inst_points in point_list:
+                # gather capsules corresponding to inst_points
+                inst_points_down16 = torch.unique(inst_points // capsule_scale, dim=1)
+
+                y_coords, x_coords = inst_points_down16[0, :], inst_points_down16[1, :]
+
+                inst_capsule_votes = capsule_votes_inst[i, :, y_coords, x_coords]  # (n_caps*vote_dim, p)
+                inst_capsule_votes = inst_capsule_votes.view(self.n_init_capsules[2], self.vote_dim, len(y_coords))  # (n_caps, vote_dim, p)
+                inst_capsule_votes = torch.transpose(inst_capsule_votes, 1, 2).reshape(self.n_init_capsules[2] * len(y_coords), self.vote_dim)  # (n_caps*p, vote_dim)
+
+                if config.positional_encoding == True:
+                    inst_points_mean = torch.mean(inst_points.float(), 0, keepdim=True)
+                    inst_points_rel = inst_points - inst_points_mean  # gets the relative coordinates
+                    y_coords_rel, x_coords_rel = inst_points_rel[0, :], inst_points_rel[1, :]
+                    y_coords_rel, x_coords_rel = y_coords_rel / float(h / capsule_scale), x_coords_rel / float(w / capsule_scale)  # Performs normalization between 0 and 1
+
+                    # x_coords_rel and y_coords_rel should be of shape (p, )
+                    x_coords_rel = x_coords_rel.unsqueeze(0).repeat(self.n_init_capsules[2], 1).reshape(self.n_init_capsules[2] * len(y_coords), )  # makes x_coords_rel of shape (N*P, )
+                    y_coords_rel = y_coords_rel.unsqueeze(0).repeat(self.n_init_capsules[2], 1).reshape(self.n_init_capsules[2] * len(y_coords), )  # makes y_coords_rel of shape (N*P, )
+
+                    if config.positional_encoding_type == 'addition':
+                        inst_capsule_votes[:, -1] += x_coords_rel.cuda()
+                        inst_capsule_votes[:, -2] += y_coords_rel.cuda()
+                    elif config.positional_encoding_type == 'concat':
+                        inst_capsule_votes = torch.cat((inst_capsule_votes, y_coords_rel.unsqueeze(1).float().cuda(), x_coords_rel.unsqueeze(1).float().cuda()), 1)
+
+                    inst_capsule_votes = self.pos_vote_transform2(inst_capsule_votes)
+
+                inst_capsule_acts = capsule_acts[i, :, y_coords, x_coords]  # (n_caps, p)
+                inst_capsule_acts = inst_capsule_acts.view(self.n_init_capsules[2] * len(y_coords), )  # (n_caps*p, )
+
+                out_capsule_poses, out_capsule_acts = self.transformer_routing2(inst_capsule_votes, inst_capsule_acts)  # (34, F_out), (34, )
+
+                class_outs.append(out_capsule_acts)
+
+            class_outputs.append(torch.stack(class_outs) if len(class_outs) != 0 else [])
+
+        return class_outputs
+
+        
+    def forward(self, x, point_lists=None, gt_seg=None, gt_reg=None, two_stage=False):
+        # (x has shape (batch_size, 3, h, w))
+        h = x.size()[2]
+        w = x.size()[3]
+
+        # Encoder:
+        feature_map, skip_8, skip_4 = self.resnet(x)  # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNe$
+
+        feature_output = self.aspp(feature_map)  # (shape: (batch_size, 1280, h/16, w/16))
+
+        (primary_poses, primary_acts), (fgbg_poses, fgbg_acts) = self.get_primary_and_fg_capsules(feature_output, skip_8, skip_4, (h, w))
+
+        fg_pred = fgbg_acts[..., 0].unsqueeze(1)  # Shape (batch_size, 1, h/16, w/16)
+
+        fg_poses = fgbg_poses[..., 0, :]
+        regressions = self.regression_linear(fg_poses).permute(0, 3, 1, 2)
+        regressions = F.tanh(regressions)
+
+        # Resizes the output maps
+        fg_pred = F.upsample(fg_pred, size=(h, w), mode="bilinear")  # (shape: (batch_size, num_classes, h, w))
+        center = torch.zeros_like(fg_pred)  # F.upsample(center, size=(h, w), mode="bilinear")
+        regressions = F.upsample(regressions, size=(h, w), mode="bilinear")
+        regressions[:, 0] = regressions[:, 0] * w
+        regressions[:, 1] = regressions[:, 1] * h
+
+        capsule_votes_class = self.vote_transform_class(capsule_poses)  # (batch_size, n_caps*vote_dim, h/16, w/16)
+
+        point_lists1, inst_maps, segmentation_lists = create_inst_maps(point_lists, gt_reg, gt_seg, fg_pred, regressions)
+        
+        class_outputs = []
+        
+        capsule_votes_inst = self.vote_transform_class(primary_poses)  # (batch_size, n_caps*vote_dim, h/16, w/16)
+        
+        instance_poses, instance_acts, class_outputs = scatter_capsules(point_lists1, capsule_votes_inst, capsule_acts, input_size, instance_scale=4)
             
+        fg_preds, reg_preds, class_preds, inst_maps_preds, seg_list_preds = [], [], [], [], []
+        
+        fg_preds.append(fg_pred)
+        reg_preds.append(regressions)
+        class_preds.append([class8to34(class_output) for class_output in class_outputs])
+        inst_maps_preds.append(inst_maps)
+        seg_list_preds.append(segmentation_lists)
+        
+        if two_stage:
+            new_capsules_poses = torch.cat((fgbg_poses.cuda(), instance_poses.cuda()), -2)
+            new_capsules_acts = torch.cat((fgbg_acts.cuda(), instance_acts.cuda()), -1)
+
+            # new_capsules_poses = new_capsules_poses.detach()
+            # new_capsules_acts = new_capsules_acts.detach()
+
+            # capsule_poses = capsule_poses.detach()
+            # capsule_acts = capsule_acts.detach()
+
+
+            new_capsules_poses = new_capsules_poses.view(b_size, h_new, w_new, -1).permute(0, 3, 1, 2)
+            capsule_votes_seg2 = self.vote_transform_seg2(new_capsules_poses)  # (batch_size, n_caps*vote_dim, h/4, w/4)
+            capsule_votes_seg3 = self.vote_transform_seg3(capsule_votes_seg2)
+            capsule_votes_seg3 = capsule_votes_seg3.permute(0, 2, 3, 1).view(b_size, h_new, w_new, self.num_inst_classes+2, self.vote_dim_seg)
+
+            fgbg_poses, fgbg_acts = self.transformer_routing_seg2(capsule_votes_seg3, new_capsules_acts)
+
+            fg_pred = fgbg_acts[..., 0].unsqueeze(1)  # Shape (batch_size, 1, h/16, w/16)
+
+            fg_poses = fgbg_poses[..., 0, :]
+            regressions = self.regression_linear(fg_poses).permute(0, 3, 1, 2)
+            regressions = F.tanh(regressions)
+
+            # Resizes the output maps
+            fg_pred = F.upsample(fg_pred, size=(h, w), mode="bilinear")  # (shape: (batch_size, num_classes, h, w))
+            center = torch.zeros_like(fg_pred)  # F.upsample(center, size=(h, w), mode="bilinear")
+            regressions = F.upsample(regressions, size=(h, w), mode="bilinear")
+            regressions[:, 0] = regressions[:, 0] * w
+            regressions[:, 1] = regressions[:, 1] * h
+
+            capsule_votes_class = self.vote_transform_class2(capsule_poses)  # (batch_size, n_caps*vote_dim, h/16, w/16)
+            capsule_votes_inst = self.vote_transform_class(primary_poses)  # (batch_size, n_caps*vote_dim, h/16, w/16)
+
+            point_lists2, inst_maps, segmentation_lists = create_inst_maps(point_lists, gt_reg, gt_seg, fg_pred, regressions)
+            
+            class_outputs = scatter_capsules2(point_lists2, capsule_votes_inst, capsule_acts, (h, w))
+                            
             fg_preds.append(fg_pred)
             reg_preds.append(regressions)
             class_preds.append([class8to34(class_output) for class_output in class_outputs])
             inst_maps_preds.append(inst_maps)
             seg_list_preds.append(segmentation_lists)
-            
-            if two_stage:
-                new_capsules_poses = torch.cat((fgbg_poses.cuda(), instance_poses.cuda()), -2)
-                new_capsules_acts = torch.cat((fgbg_acts.cuda(), instance_acts.cuda()), -1)
-
-                new_capsules_poses = new_capsules_poses.detach()
-                new_capsules_acts = new_capsules_acts.detach()
-
-                capsule_poses = capsule_poses.detach()
-                capsule_acts = capsule_acts.detach()
-
-
-                new_capsules_poses = new_capsules_poses.view(b_size, h_new, w_new, -1).permute(0, 3, 1, 2)
-                capsule_votes_seg2 = self.vote_transform_seg2(new_capsules_poses)  # (batch_size, n_caps*vote_dim, h/4, w/4)
-                capsule_votes_seg3 = self.vote_transform_seg3(capsule_votes_seg2)
-                capsule_votes_seg3 = capsule_votes_seg3.permute(0, 2, 3, 1).view(b_size, h_new, w_new, self.num_inst_classes+2, self.vote_dim_seg)
-
-                fgbg_poses, fgbg_acts = self.transformer_routing_seg2(capsule_votes_seg3, new_capsules_acts)
-
-                fg_pred = fgbg_acts[..., 0].unsqueeze(1)  # Shape (batch_size, 1, h/16, w/16)
-
-                fg_poses = fgbg_poses[..., 0, :]
-                regressions = self.regression_linear(fg_poses).permute(0, 3, 1, 2)
-                regressions = F.tanh(regressions)
-
-                # Resizes the output maps
-                fg_pred = F.upsample(fg_pred, size=(h, w), mode="bilinear")  # (shape: (batch_size, num_classes, h, w))
-                center = torch.zeros_like(fg_pred)  # F.upsample(center, size=(h, w), mode="bilinear")
-                regressions = F.upsample(regressions, size=(h, w), mode="bilinear")
-                regressions[:, 0] = regressions[:, 0] * w
-                regressions[:, 1] = regressions[:, 1] * h
-
-                capsule_votes_class = self.vote_transform_class2(capsule_poses)  # (batch_size, n_caps*vote_dim, h/16, w/16)
-
-                if point_lists is None:
-                    # inst_maps, point_lists, segmentation_lists = self.hough_routing(fg_pred, regressions, gt_seg)
-                    if gt_reg is None:
-                        inst_maps, point_lists2, segmentation_lists = self.hough_routing(fg_pred, regressions, gt_seg)
-                    else:
-                        inst_maps, point_lists2, segmentation_lists = self.hough_routing(fg_pred, gt_reg, gt_seg)
-                else:
-                    inst_maps = []
-                    segmentation_lists = []
-                    point_lists2 = point_lists
-
-                class_outputs = []
-                
-                for i, point_list in enumerate(point_lists2):
-
-                    class_outs = []
-                    for inst_points in point_list:
-                        # gather capsules corresponding to inst_points
-                        inst_points = torch.unique(inst_points // 16, dim=1)
-
-                        y_coords, x_coords = inst_points[0, :], inst_points[1, :]
-
-                        inst_capsule_votes = capsule_votes_class[i, :, y_coords, x_coords]  # (n_caps*vote_dim, p)
-                        inst_capsule_votes = inst_capsule_votes.view(self.n_init_capsules[2], self.vote_dim, len(y_coords))  # (n_caps, vote_dim, p)
-                        inst_capsule_votes = torch.transpose(inst_capsule_votes, 1, 2).reshape(self.n_init_capsules[2]*len(y_coords), self.vote_dim)  # (n_caps*p, vote_dim)
-
-                        #assert config.positional_encoding == False  # positional encoding will require more changes
-                        if config.positional_encoding == True:
-                            inst_points_mean = torch.mean(inst_points.float(), 0, keepdim=True)
-                            inst_points_rel = inst_points - inst_points_mean  # gets the relative coordinates
-                            y_coords_rel, x_coords_rel = inst_points_rel[0, :], inst_points_rel[1, :]
-                            y_coords_rel, x_coords_rel = y_coords_rel / float(h / 16), x_coords_rel / float(w / 16)  # Performs normalization between 0 and 1
-
-                            # x_coords_rel and y_coords_rel should be of shape (p, )
-                            x_coords_rel = x_coords_rel.unsqueeze(0).repeat(self.n_init_capsules[2], 1).reshape(self.n_init_capsules[2]*len(y_coords), )  # makes x_coords_rel of shape (N*P, )
-                            y_coords_rel = y_coords_rel.unsqueeze(0).repeat(self.n_init_capsules[2], 1).reshape(self.n_init_capsules[2]*len(y_coords), )  # makes y_coords_rel of shape (N*P, )
-
-                            if config.positional_encoding_type == 'addition':
-                                inst_capsule_votes[:, -1] += x_coords_rel.cuda()
-                                inst_capsule_votes[:, -2] += y_coords_rel.cuda()
-                            elif config.positional_encoding_type == 'concat':
-                                inst_capsule_votes = torch.cat((inst_capsule_votes, y_coords_rel.unsqueeze(1).float().cuda(), x_coords_rel.unsqueeze(1).float().cuda()), 1)
-
-                            inst_capsule_votes = self.pos_vote_transform2(inst_capsule_votes)
-
-                        inst_capsule_acts = capsule_acts[i, :, y_coords, x_coords]    # (n_caps, p)
-                        inst_capsule_acts = inst_capsule_acts.view(self.n_init_capsules[2]*len(y_coords), )  # (n_caps*p, )
-
-                        out_capsule_poses, out_capsule_acts = self.transformer_routing2(inst_capsule_votes, inst_capsule_acts)  # (34, F_out), (34, )
-                        
-                        class_outs.append(out_capsule_acts)
-
-                    class_outputs.append(torch.stack(class_outs) if len(class_outs) != 0 else [])
-                    
-                    fg_preds, reg_preds, class_preds, inst_maps_preds, seg_list_preds = [], [], [], [], []
-                    
-                    fg_preds.append(fg_pred)
-                    reg_preds.append(regressions)
-                    class_preds.append([class8to34(class_output) for class_output in class_outputs])
-                    inst_maps_preds.append(inst_maps)
-                    seg_list_preds.append(segmentation_lists)
             
 
         # Should output center with shape (B, 1, H/16, W/16)
@@ -674,4 +694,3 @@ class CapsuleModel6(nn.Module):
         if not os.path.exists(self.model_dir):
             os.makedirs(self.model_dir)
             os.makedirs(self.checkpoints_dir)
-
