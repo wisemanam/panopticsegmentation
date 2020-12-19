709f473800a038dcaf75608d118824dedaa91b75
Audrey Wiseman <66831415+wisemanam@users.noreply.github.com>
2020-12-19 16:59:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,17 @@ n_iterations = 120000
 
 save_every_n_iters = 1000
 
-batch_size = 8
+batch_size = 12
 
 n_classes = 91
 
 model = 'CapsuleModel4'
 
+# model_id = 1 # CapsuleModel6, two_stage=False
+# model_id = 2 # CapsuleModel6, two_stage, gradient stopped
+# model_id = 3 # NewCapsuleModel6, two_stage, stop_grad = True
+# model_id = 4 # NewCapsuleModel6, stop_grad = False
+# model_id = 5 # CapsuleModel7
 model_id = 6 # CapsuleModel4 with COCO
 
 save_dir = './SavedModels/Run%d/' % model_id
@@ -42,6 +47,6 @@ seg_coef = 1.0
 regression_coef = 1.0
 class_coef = 1.0
 
-h, w = 512, 1024
+h, w = 640, 640
 
 use_instance = True
